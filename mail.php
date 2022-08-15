377949b441770c05506a905112ab5352cbef5685
Abairaj <111057814+Abairaj@users.noreply.github.com>
2022-08-15 18:02:21 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,46 +1,35 @@
 <?php
-//get data from form  
 
-$errors = [];
-$errorMessage = '';
+// VALUES FROM THE FORM
 
-if (!empty($_POST)) {
-    $name = $_POST['name'];
-    $number = $_POST['number'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    if (empty($name)) {
-        $errors[] = 'Name is empty';
-    }
-
-    if (empty($email)) {
-        $errors[] = 'Email is empty';
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Email is invalid';
-    }
-
-    if (empty($message)) {
-        $errors[] = 'Message is empty';
-    }
+$name      = $_POST['name'];
+$number      = $_POST['number'];
+$email      = $_POST['email'];
+$message        = $_POST['message'];
 
 
-    if (empty($errors)) {
-        $toEmail = 'abairajpoinachi@gmail.com';
-        $emailSubject = 'New email from your contant form';
-        $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
+// CREATE THE EMAIL
 
-        $bodyParagraphs = ["Name: {$name}","Number: {$number}", "Email: {$email}", "Message:", $message];
-        $body = join(PHP_EOL, $bodyParagraphs);
+$headers    = "Content-Type: text/plain; charset=iso-8859-1\n";
 
-        if (mail($toEmail, $emailSubject, $body, $headers)) {
-            header('Location: thank-you.html');
-        } else {
-            $errorMessage = 'Oops, something went wrong. Please try again later';
-        }
-    } else {
-        $allErrors = join('<br/>', $errors);
-        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
-    }
-}
+$headers    = "From: $name <$email>\n";
+
+$recipient  = "abairajpoinachi@gmail.com";
+
+$subject    = "form";
+
+$message    = wordwrap($name, $number, $email, $message , 1024);
+
+
+
+// SEND THE EMAIL TO YOU
+
+mail($recipient, $subject, $message, $headers);
+
+
+
+// REDIRECT TO THE THANKS PAGE
+
+header("location: thanks.php");
+
 ?>
