83c6d3d98cd70f09491a0a265e786fea7b474d5c
Abairaj <111057814+Abairaj@users.noreply.github.com>
2022-08-15 18:06:51 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -12,7 +12,7 @@ $message        = $_POST['message'];
 
 $headers    = "Content-Type: text/plain; charset=iso-8859-1\n";
 
-$headers    = "From: $email\n";
+$headers    = "From:abairaj.github.io/dreamrich.github.io\n";
 
 $recipient  = "abairajpoinachi@gmail.com";
 
