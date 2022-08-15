9898a2a8d3f2d6f26dbc111a7769a2cd5fd162ce
Abairaj <111057814+Abairaj@users.noreply.github.com>
2022-08-15 16:56:39 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$number= $_POST['number'];
+$email= $_POST['email address'];
+$message= $_POST['message'];
+$to = "abairajpoinachi@gmail.com";
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n Mobile number=".$number. "\r\n Email = " . $email . "\r\n Message =" . $message;
+
+$headers = "From: arkclickscm@gmail.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
+?>
